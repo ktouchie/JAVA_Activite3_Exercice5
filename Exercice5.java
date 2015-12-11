fd4e79ce1341e311afe2bc1604e42764b3a6019e
Karin Touchie <ktouchie@gmail.com>
2015-12-11 18:02:07 +0100

    Add while loop for factorielle

---
+++ Exercice5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Exercice5 {
+    
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Veuillez saisir un nombre entier positif:");
+            int n = sc.nextInt();
+        
+        if (n==0) {
+            System.out.println("La factorielle de " + n + " est 1.");
+        } else {
+            while (n>0) {
+                var x=n*(n-1);
+                n=(n-1);
+            }
+            System.out.println(x);
+        }
+    }
+}
